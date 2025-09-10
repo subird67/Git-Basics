326a674a07b9e71f0a5d9798670fcca23dbaef34
Subir Das <subird67@gmail.com>
2025-09-10 13:27:50 +0530

    Revert back deleted code

--- sample.py
+++ sample.py
@@ -1,5 +1,10 @@
 a = 2
 b = 3
 print(a+b)
+<<<<<<< HEAD
 
 sample = "Sum of a and b"
+=======
+i = int(input("Enter a No"))
+k = int(input("Enter your name"))
+>>>>>>> parent of b2feae7 (Remove few codes)
