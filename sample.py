89ae4a6e57ff2f6ef68cf53d234c4db43656f2bd
Subir Das <subird67@gmail.com>
2025-09-17 19:47:09 +0530

    Add features and modify previous code

--- sample.py
+++ sample.py
@@ -1,11 +1,12 @@
 a = 2
 b = 3
 print(a+b)
-<<<<<<< HEAD
 
 sample = "Sum of a and b"
-=======
+
 i = int(input("Enter a No"))
 j = int(input("Enter your age"))
 k = int(input("Enter your name"))
 print(i,j,k)
+
+print("Subir is a DevOps Engineer")
